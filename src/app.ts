98b482f1147079ae3222075df4b739e111bd2cbe
tejasmahajan2 <tejas.mahajan@in.spundan.com>
2025-02-01 11:03:51 +0530

    added required body validation

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+
 connectDB();
 
 const ProjectSchema = new mongoose.Schema({
--- src/common/controllers/generic-crud.controller.ts
+++ src/common/controllers/generic-crud.controller.ts
@@ -10,15 +10,10 @@ export class GenericController<T extends Document> {
   public getRoutes(): express.Router {
     let appRoutes = express.Router({ mergeParams: true });
 
-    appRoutes.post('/', async (req: Request, res: Response, next: NextFunction) => {
+    appRoutes.post('/', this.validateSchema, async (req: Request, res: Response, next: NextFunction) => {
       try {
-        if (req.body) {
-          res.status(401).json({ error: 'Invalid data.' });
-        } else {
-          console.log(req.body);
-          const data = await this.service.create(req.body);
-          res.status(201).json(data);
-        }
+        const data = await this.service.create(req.body);
+        res.status(201).json(data);
       } catch (error) {
         res.status(400).json({ error: 'Internal server error' });
       }
@@ -43,13 +38,13 @@ export class GenericController<T extends Document> {
       }
     });
 
-    appRoutes.put('/:id', this.validateId, async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    appRoutes.put('/:id', this.validateId, this.validateSchema, async (req: Request, res: Response, next: NextFunction): Promise<any> => {
       try {
         const data = await this.service.update(req.params.id, req.body);
-        if (!data) return res.status(404).json({ message: "Not found" });
+        if (!data) return res.status(404).json({ message: `Not found` });
         res.json(data);
       } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
+        res.status(500).json({ error: `Unexpected error occured during update.` });
       }
     });
 
@@ -70,4 +65,17 @@ export class GenericController<T extends Document> {
     if (!Types.ObjectId.isValid(req.params.id)) res.status(404).json({ message: "Invalid object id." });
     else next();
   }
+
+  validateSchema(req: Request, res: Response, next: NextFunction) {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      res.status(400).json({ error: "Request body is empty!" });
+    } else {
+      next();
+    }
+  }
+
+  private extractSecondSegment(url: string) {
+    const match = url.match(/^(?:\/?[^\/]+)?\/([^\/]+)/);
+    return match ? match[1] : null;
+  };
 }
